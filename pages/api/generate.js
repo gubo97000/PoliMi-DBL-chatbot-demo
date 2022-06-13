8a5740787b07707038300cc2d6c67ae4842f6119
gubo97000 <gubo97000@hotmail.it>
2022-06-13 23:24:06 +0200

    first commit

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,28 +1,45 @@
 import { Configuration, OpenAIApi } from "openai";
-
+// import data from "../public/data";
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+  console.log(generatePrompt(req.body.chatHistory))
+  const completion = await openai.createCompletion("text-davinci-002", {
+    prompt: generatePrompt(req.body.chatHistory),
+    temperature: 0.7,
+    max_tokens: 500,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0.6,
   });
-  res.status(200).json({ result: completion.data.choices[0].text });
+  res.status(200).json({ result: completion.data.choices[0].text, data:completion.data });
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+function generatePrompt(hist) {
+  const preamble = `Questa è una conversazione con Rob l'assistente virtuale di Mediolanum, una banca in Italia, la sua funzione principale è aiutare i dipendenti della banca a migliorare il loro wellbeing, suggerendo utili consigli sia lato fisico, sia mentale. I dipendenti possono lavorare o nel campus di Basiglio quindi partecipare agli eventi di quel giorno o lavorare in remoto.`
+  const info = `Impiegato Paolo:
+Attività Odierne{Lavoro in remoto, 5 riunioni online, 4000 passi, 1 ora di postura scorretta}
+Attività di domani{Lavoro in campus, 3 riunioni online, 5 di presenza}
+
+Eventi al campus Basiglio:
+-Mozzarella party, domani alle 17:00
+-Aperitivo, domani alle 18:00
+Eventi disponibili online: 
+-Postural training
+-Classe di meditazione
+-Classe di yoga
+Servizi in campus:
+-Psicologo
+-Ristorante
+`
+  return `${preamble} ${info}
+${hist.slice(-8).map((h)=>{return `${h.sender}: ${h.message}`}).join("\n")}
+Rob: `
 }
+
+
+
--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,108 @@
+import SendIcon from '@mui/icons-material/Send';
+import { Avatar } from "@mui/material";
+import LoadingButton from '@mui/lab/LoadingButton';
 import Head from "next/head";
-import { useState } from "react";
+import {useState } from "react";
 import styles from "./index.module.css";
 
+function ChatBubbleComponent({ msg }) {
+  return (<><div style={{
+    width: "100%",
+    // margin: "10px",
+    display: "flex",
+    flexDirection: msg.sender == 'Rob' ? "row" : "row-reverse"
+  }}>
+    <Avatar style={{
+      margin: `${!(msg.sender == 'Rob') ? "0 0 0 10px" : "0 10px 0 0"}`
+    }} src={(msg.sender == 'Rob') ? '/rob.png' : ""} ></Avatar>
+    <div style={{
+      border: '1px solid rgba(0, 0, 0, 0.12)',
+      borderRadius: "10px",
+      padding: "10px",
+      maxWidth: "250px"
+    }}> {msg.message} </div>
+  </div></>);
+}
+
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
-  const [result, setResult] = useState();
+  const [chatHistory, setChatHistory] = useState([
+    { sender: "Rob", message: "Ciao Paolo! Come è andata oggi?" },
+    // { sender: "Paolo", message: "Ciao Rob bene, ma è stata un po' stressante la giornata" },
+    // { sender: "Rob", message: "Hai lavorato molto oggi, hai avuto 4 riuinioni su teams!" },
+  ]);
+  const [input, setInput] = useState('');
+  const [fetching, setFetching] = useState(false);
 
   async function onSubmit(event) {
     event.preventDefault();
-    const response = await fetch("/api/generate", {
+    if (input.trim().length === 0) return;
+    setFetching(true);
+    const fetchPromise = fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ chatHistory: [...chatHistory, { sender: "Paolo", message: input }] }), //State change is async only doable like this
     });
+
+    setChatHistory(hist => [...hist, { sender: "Paolo", message: input }]);
+    setInput('');
+
+    const response = await fetchPromise;
     const data = await response.json();
-    setResult(data.result);
-    setAnimalInput("");
+    setFetching(false);
+    setChatHistory(hist => [...hist, { sender: "Rob", message: data.result }]);
+    console.log(data);
   }
 
   return (
-    <div>
+    <>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Mediolanum U</title>
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Mediolanum U - Chat with Rob</h3>
+        <div style={{
+          overflowY: "auto",
+          overflowX: "hidden",
+          display: "flex",
+          flexDirection: "column-reverse",
+          width: "400px",
+        }}>
+
+          <div style={{
+            display: "flex",
+            flexDirection: 'column-reverse',
+            gap: "20px",
+            height: "450px",
+            width: "100%",
+            overflowY: "scroll",
+            marginBottom: "20px"
+          }}>
+            {chatHistory.slice().reverse().map((msg, i) => { return <ChatBubbleComponent msg={msg} key={i} /> })}
+          </div>
+        </div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="message"
+            placeholder="Manda un messaggio"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+
           />
-          <input type="submit" value="Generate names" />
+          <LoadingButton
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={onSubmit} disabled={!input || fetching}
+            loading={fetching}
+            loadingPosition="end"
+          >
+            Invia
+          </LoadingButton>
         </form>
-        <div className={styles.result}>{result}</div>
       </main>
-    </div>
+    </>
   );
 }
